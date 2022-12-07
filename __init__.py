939ae47be4da784335a24bb98c601d5d132915d9
Sean Deaton <sean.deaton@me.com>
2022-12-06 21:17:21 -0500

    Add subtree on the plugin command.
    This will allow for using LLIL, MLIL, and Pseudo-C later on.

--- __init__.py
+++ __init__.py
@@ -1,9 +1,9 @@
 from binaryninja import PluginCommand
 from . src.entry import check_function
 
-PluginCommand.register_for_high_level_il_function("What Does this Function Do?",
-                            "Checks OpenAI to see what this function does." \
+PluginCommand.register_for_high_level_il_function("OpenAI\What Does this Function Do (HLIL)?",
+                            "Checks OpenAI to see what this HLIL function does." \
                             "Requires an internet connection and an API key "
                             "saved under the environment variable "
-                            "OPENAI_API_KEY",
+                            "OPENAI_API_KEY or modify the path in entry.py.",
                             check_function)
