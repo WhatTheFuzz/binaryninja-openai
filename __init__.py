3dc8aa104acaf7199aa6e61b90f8ed7e17b7e90d
Sean Deaton <sean.deaton@me.com>
2022-12-13 10:45:29 -0500

    Add PluginCommand to submit the decompiled C.
    Remove print statement that would print None; printing to the
    console now takes place in the BackgroundTaskThread.
    
    Relate #8.

--- __init__.py
+++ __init__.py
@@ -11,3 +11,10 @@ PluginCommand.register_for_high_level_il_function("OpenAI\What Does this Functio
                             "saved under the environment variable "
                             "OPENAI_API_KEY or modify the path in entry.py.",
                             check_function)
+
+PluginCommand.register_for_function("OpenAI\What Does this Function Do (Pseudo-C)?",
+                            "Checks OpenAI to see what this pseudo-C function does." \
+                            "Requires an internet connection and an API key "
+                            "saved under the environment variable "
+                            "OPENAI_API_KEY or modify the path in entry.py.",
+                            check_function)
